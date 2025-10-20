58dd7167221d2b502205654fe8879a6a3654d8a6
eudocio-washimi <eudocio.washimi@fatec.sp.gov.br>
2025-10-20 08:21:44 -0300

    Ajustes na página de login e no código de cadastro

---
+++ frm_login.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_login
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txt_usuario = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt_senha = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txt_usuario
+        '
+        Me.txt_usuario.Location = New System.Drawing.Point(51, 73)
+        Me.txt_usuario.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txt_usuario.Name = "txt_usuario"
+        Me.txt_usuario.Size = New System.Drawing.Size(378, 26)
+        Me.txt_usuario.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(47, 47)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 20)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Usuário"
+        '
+        'txt_senha
+        '
+        Me.txt_senha.Location = New System.Drawing.Point(51, 151)
+        Me.txt_senha.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txt_senha.Name = "txt_senha"
+        Me.txt_senha.Size = New System.Drawing.Size(378, 26)
+        Me.txt_senha.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(47, 125)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 20)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Senha"
+        '
+        'frm_login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(472, 223)
+        Me.Controls.Add(Me.txt_senha)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_usuario)
+        Me.Controls.Add(Me.Label3)
+        Me.Name = "frm_login"
+        Me.Text = "frm_login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txt_usuario As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txt_senha As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ frm_login.vb
@@ -0,0 +1,5 @@
+﻿Public Class frm_login
+    Private Sub txt_usuario_TextChanged(sender As Object, e As EventArgs) Handles txt_usuario.TextChanged
+
+    End Sub
+End Class
